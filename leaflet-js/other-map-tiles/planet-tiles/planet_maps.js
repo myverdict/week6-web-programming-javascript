4b46f5ca348dba63bc8cd9f16933d5497170fc2a
Sam Vitare <sandhya.palanki@gmail.com>
2023-09-18 15:01:51 -0500

    updated

--- leaflet-js/other-map-tiles/planet-tiles/planet_maps.js
+++ leaflet-js/other-map-tiles/planet-tiles/planet_maps.js
@@ -6,24 +6,25 @@
 // NOTE: for the div id 'college-mapid', no # is required
 
 // Create the map for the div element with ID mars-mapid
-let marsMap = L.map('mars-mapid')
+let marsMap = L.map("mars-mapid");
 
 // The valid zoom levels are a smaller range than for Earth
-marsMap.setView([0, 0], 3)   
+marsMap.setView([0, 0], 3);
 
 // Add the tile layer - rocks, craters, labels etc. Without this, nothing to see
-L.tileLayer('https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-2/all/{z}/{x}/{y}.png')
-    .addTo(marsMap)
-    .setZIndex(0)
-
+L.tileLayer(
+  "https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-mars-basemap-v0-2/all/{z}/{x}/{y}.png"
+).addTo(marsMap)
+  .setZIndex(0);
 
 // Create the map for the div element with ID moon-mapid
-let moonMap = L.map('moon-mapid')
+let moonMap = L.map("moon-mapid");
 
 // The valid zoom levels are a smaller range than for Earth
-moonMap.setView([0, 0], 3)
+moonMap.setView([0, 0], 3);
 
 // Add the tile layer - rocks, craters, labels etc. Without this, nothing to see
-let baselayer = new L.tileLayer('https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png')
-    .addTo(moonMap)
-    .setZIndex(0);
+let baselayer = new L.tileLayer(
+  "https://cartocdn-gusc.global.ssl.fastly.net/opmbuilder/api/v1/map/named/opm-moon-basemap-v0-1/all/{z}/{x}/{y}.png"
+).addTo(moonMap)
+  .setZIndex(0);
